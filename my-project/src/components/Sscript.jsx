141f6c63d3f9a4c2b990a39ce2856426ca03090b
Tolga Akif Gönen <akiftolgagonen@gmail.com>
2023-09-14 00:35:03 +0300

    v4
    works fine but ugly , missing pages

--- my-project/src/components/Sscript.jsx
+++ my-project/src/components/Sscript.jsx
@@ -13,12 +13,6 @@ function formatDate(date) {
   return `${month}/${day}/${year}`;
 }
 
-const Datepicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-  );
-};
 
 export const Sscript1 = () => {
   const [selectedCity1, setSelectedCity1] = useState(null);
@@ -33,20 +27,17 @@ export const Sscript1 = () => {
 
   const handleDateChange1 = (date) => {
     setSelectedDate1(date);
-    const formattedDate1 = formatDate(selectedDate1);
   };
   const [selectedDate2, setSelectedDate2] = useState(new Date());
 
   const handleDateChange2 = (date) => {
     setSelectedDate2(date);
-    const formattedDate2 = formatDate(selectedDate2);
   };
+
   let cityhelp1 = "";
   let cityhelp2 = "";
-  let dateHelp1 = "";
-  let dateHelp2 = "";
-  let dateF1 = "";
-  let dateF2 = "";
+  let dateHelp1 = formatDate(selectedDate1);
+  let dateHelp2 = formatDate(selectedDate2);
 
   return (
     <div>
@@ -74,19 +65,17 @@ export const Sscript1 = () => {
       </div>
       <div className="pb-4">
         <div className="border border-gray-400 flex px-3 py-3 text-black rounded-xl">
-          <Datepicker onChange={handleDateChange1} />
-          <h1 className="text-white">{(dateF1 = formatDate(selectedDate1))}</h1>
-          <h1 className="text-white">{(dateHelp1 = dateF1)}</h1>
+          <DatePicker selected={selectedDate1} onChange={handleDateChange1} />
+          <h1 className="text-white">{dateHelp1}</h1>
         </div>
       </div>
       <div className="pb-4">
         <div className="border border-gray-400 flex px-3 py-3 text-black rounded-xl">
-          <Datepicker onChange={handleDateChange2} />
-          <h1 className="text-white">{(dateF2 = formatDate(selectedDate2))}</h1>
-          <h1 className="text-white">{(dateHelp2 = dateF2)}</h1>
+          <DatePicker  selected={selectedDate2} onChange={handleDateChange2} />
+          <h1 className="text-white">{dateHelp2}</h1>
         </div>
       </div>
-      <button class="bg-[#9cb034] hover:bg-[#e4ff5d] text-black font-bold py-2 px-4 border-b-4 border-[#757e43] hover:border-[#dbef78] rounded">
+      <button className="bg-[#9cb034] hover:bg-[#e4ff5d] text-black font-bold py-2 px-4 border-b-4 border-[#757e43] hover:border-[#dbef78] rounded">
         <Link
           to={`/bilet?cityGo=${cityhelp1}&cityTurn=${cityhelp2}&dateF1=${dateHelp1}&dateF2=${dateHelp2}`}
         >
@@ -114,8 +103,8 @@ export const Sscript2 = () => {
   };
   let cityhelp1 = "";
   let cityhelp2 = "";
-  let dateHelp = "";
-  let dateF1 = "";
+  let dateHelp = formatDate(selectedDate3);
+ 
 
   return (
     <div>
@@ -143,12 +132,10 @@ export const Sscript2 = () => {
       </div>
       <div className="pb-4">
         <div className="border border-gray-400 flex px-3 py-3 text-black rounded-xl">
-          <Datepicker onChange={handleDateChange3} />
-          <h1 className="text-white">{(dateF1 = formatDate(selectedDate3))}</h1>
-          <h1 className="text-white">{(dateHelp = dateF1)}</h1>
+          <DatePicker selected={selectedDate3} onChange={handleDateChange3} />
         </div>
       </div>
-      <button class="bg-[#9cb034] hover:bg-[#e4ff5d] text-black font-bold py-2 px-4 border-b-4 border-[#757e43] hover:border-[#dbef78] rounded">
+      <button className="bg-[#9cb034] hover:bg-[#e4ff5d] text-black font-bold py-2 px-4 border-b-4 border-[#757e43] hover:border-[#dbef78] rounded">
         <Link
           to={`/bilet?cityGo=${cityhelp1}&cityTurn=${cityhelp2}&dateF1=${dateHelp}&dateF2=${null}`}
         >
